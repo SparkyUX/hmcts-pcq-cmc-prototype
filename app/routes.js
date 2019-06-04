3837b87594078ae3b19ce3772051a9b66b391aa6
Jen Sparks <jen.sparks@justice.gov.uk>
2019-06-04 08:55:44 +0100

    integrate cmc

--- app/routes.js
+++ app/routes.js
@@ -8,31 +8,43 @@ router.post('/disability-answer', function (req, res) {
   let disabilityInformation = req.session.data['disability-information']
 
   if (disabilityInformation == 'Yes') {
-    res.redirect('/CMC-Q-disability-yes')
+    res.redirect('/disability/disability-yes')
   } else {
-    res.redirect('/CMC-Q-religion-or-belief')
+    res.redirect('/religion-or-belief')
   }
 
 })
 
+router.post('/disability-details', function (req, res) {
+
+  let disabilityYes = req.session.data['disability-yes']
+
+  if (disabilityYes == 'Yes, limited a little' || disabilityYes == 'Yes, limited a lot') {
+    res.redirect('/disability/disability-yes-detail')
+  }
+  else {
+    res.redirect('/religion-or-belief')
+  }
+
+})
 
 router.post('/ethnicity-answer', function (req, res) {
   let ethnicGroup = req.session.data['ethnic-group']
 
   if (ethnicGroup == "Prefer not to say") {
-    res.redirect('/CMC-Q-pregnancy')
+    res.redirect('/pregnancy')
   } else if (ethnicGroup == "White") {
-    res.redirect('/ethnic-group/CMC-Q-ethnicity-white')
+    res.redirect('/ethnic-group/ethnicity-white')
   } else if (ethnicGroup == "Mixed or multiple ethnic groups") {
-    res.redirect('/ethnic-group/CMC-Q-ethnicity-mixed')
+    res.redirect('/ethnic-group/ethnicity-mixed')
   } else if (ethnicGroup == "Asian or Asian British") {
-    res.redirect('/ethnic-group/CMC-Q-ethnicity-asian')
+    res.redirect('/ethnic-group/ethnicity-asian')
   } else if (ethnicGroup == "Black, African, Black British or Carribean") {
-    res.redirect('/ethnic-group/CMC-Q-ethnicity-black')
+    res.redirect('/ethnic-group/ethnicity-black')
   } else if (ethnicGroup == "Another ethnic group") {
-    res.redirect('/ethnic-group/CMC-Q-ethnicity-arab')
+    res.redirect('/ethnic-group/ethnicity-another')
   } else {
-    res.redirect('/CMC-Q-pregnancy.html')
+    res.redirect('/pregnancy.html')
   }
 
 })
